46c51b3ae8904f2fed32b77831c9ced1daf30d65
Ganesh Ledange <ganeshledange2020@gmail.com>
2025-07-31 13:44:48 +0530

    Add Databinding component

--- src/components/data-binding/data-binding.jsx
+++ src/components/data-binding/data-binding.jsx
@@ -1,14 +1,27 @@
-
+import { useEffect, useState } from "react"
 
 export function DataBinding(){
+    
+    const [price, setPrice] = useState(0);
+    const [name, setName] = useState('');
+    const [views] = useState(5600009);
+    
+    useEffect(()=>{
+        setPrice(40500);
+        setName('Samsung TV');
+    },[]);
 
-    var uname = "John";
-    var bgstyle = "bg-dark";
     return(
         <div className="container-fluid">
-            <h2 className={`text-warning ${bgstyle}`}>Data Binding</h2>
-            <p>Hello ! {uname}</p>
-            <input type="text" value={uname} />
+            <h2 className={`text-center text-warning p-3`}>Data Binding</h2>
+            <dl>
+                <dt>Product Name</dt>
+                <dd>{name}</dd>
+                <dt>Price</dt>
+                <dd>{price}</dd>
+                 <dt>Views</dt>
+                <dd>{views.toLocaleString('en-us', {notation:'compact'})}</dd>
+            </dl>
         </div>
     )
 }
